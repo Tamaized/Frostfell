7a0367a0e5c7b2ee4f2bb3f4245b21343243e1b0
Tamaized <ryanolt123@gmail.com>
2019-01-03 23:28:50 -0500

    missed some shit

--- src/main/java/Frostfell.java
+++ src/main/java/Frostfell.java
@@ -3,10 +3,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Frostfell.MODID)
+@Mod(modid = Frostfell.MODID, version = Frostfell.VERSION, acceptedMinecraftVersions = "[1.12,)")
 public class Frostfell {
 
 	public static final String MODID = "frostfell";
+	public static final String VERSION = "${version}";
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
