e292dec19689eeeeb217d3db7859d2a8070d73a4
Tamaized <ryanolt123@gmail.com>
2019-01-03 23:26:35 -0500

    inital files

---
+++ src/main/java/Frostfell.java
@@ -0,0 +1,26 @@
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Frostfell.MODID)
+public class Frostfell {
+
+	public static final String MODID = "frostfell";
+
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+
+	}
+
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event) {
+
+	}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+
+	}
+
+}
